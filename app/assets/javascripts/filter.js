23a31194c32809093991773c99095cb34bb7523a
ToniRib <tonimarierib@gmail.com>
2016-02-04 17:24:53 -0700

    Fix javascript calls and move some javascript to new file

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -1,47 +1,15 @@
 $(document).ready(function () {
-  // var $artists = $('.artist');
-  // var $categories = $('.category');
-  var $bids = $('.bid');
-  var $listings = $('.listing');
+  var $bids = $(".bid");
+  var $listings = $(".listing");
 
-  // $('#artist_filter_name').on('keypress', function () {
-  //   var currentName = this.value.toLowerCase();
-  //
-  //   $artists.each(function (index, artist) {
-  //     var $artist = $(artist);
-  //     var $artistName = $artist.find('h2 span').text().toLowerCase();
-  //
-  //     if ($artistName.startsWith(currentName)) {
-  //       $artist.show();
-  //     } else {
-  //       $artist.hide();
-  //     }
-  //   });
-  // });
-  //
-  // $('#category_filter_name').on('keypress', function () {
-  //   var currentName = this.value.toLowerCase();
-  //
-  //   $categories.each(function (index, category) {
-  //     var $category = $(category);
-  //     var $categoryName = $category.find('h2').text().toLowerCase();
-  //
-  //     if ($categoryName.startsWith(currentName)) {
-  //       $category.show();
-  //     } else {
-  //       $category.hide();
-  //     }
-  //   });
-  // });
-
-  $('#bid_status').on('change', function () {
-    var currentStatus = $('#bid_status :selected').text();
+  $("#bid_status").on("change", function () {
+    var currentStatus = $("#bid_status :selected").text();
 
     $bids.each(function (index, bid) {
       var $bid = $(bid);
-      var $bidStatus = $bid.find('.bid-status').text();
+      var $bidStatus = $bid.find(".bid-status").text();
 
-      if ($bidStatus === currentStatus || currentStatus === 'all') {
+      if ($bidStatus === currentStatus || currentStatus === "all") {
         $bid.show();
       } else {
         $bid.hide();
@@ -49,14 +17,14 @@ $(document).ready(function () {
     });
   });
 
-  $('#job_status').on('change', function () {
-    var currentStatus = $('#job_status :selected').text();
+  $("#job_status").on("change", function () {
+    var currentStatus = $("#job_status :selected").text();
 
     $listings.each(function (index, listing) {
       var $listing = $(listing);
-      var $jobStatus = $listing.find('.job-status').text();
+      var $jobStatus = $listing.find(".job-status").text();
 
-      if ($jobStatus === currentStatus || currentStatus === 'all') {
+      if ($jobStatus === currentStatus || currentStatus === "all") {
         $listing.show();
       } else {
         $listing.hide();
