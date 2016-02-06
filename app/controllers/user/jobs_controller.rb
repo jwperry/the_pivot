77c2eae9085cd6f5437fd4e9af059a64843ab1fe
ToniRib <tonimarierib@gmail.com>
2016-02-06 11:12:25 -0700

    Update bid form to use new duration estimates dropdown

--- app/controllers/user/jobs_controller.rb
+++ app/controllers/user/jobs_controller.rb
@@ -1,4 +1,17 @@
 class User::JobsController < ApplicationController
+  before_action :set_duration_tags, only: [:show]
+
   def show
+  end
+
+  private
+
+  def set_duration_tags
+    @duration_tags = [
+      ["Short (1 week or less)", 0],
+      ["Medium (1 - 4 weeks)", 1],
+      ["Long (Longer than 4 weeks)", 2],
+      ["Event (Specific date & time)", 3]
+    ]
   end
 end
