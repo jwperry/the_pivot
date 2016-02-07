aaa985d579e8d938fafa3256e59b5c5fe70e9a3e
Joe Perry <joseph.w.perry@gmail.com>
2016-02-07 11:19:42 -0700

    Testing and functionality for profile edit finished

--- test/integration/lister_can_edit_profile_test.rb
+++ test/integration/lister_can_edit_profile_test.rb
@@ -5,13 +5,13 @@ class ListerCanEditProfileTest < ActionDispatch::IntegrationTest
     lister = create(:lister)
     original_lister = lister.dup
     visit root_path
-    
-    within("#nav-mobile") do
+
+    within "#nav-mobile" do
       click_on "Login"
     end
     fill_in "Username", with: lister.username
     fill_in "Password", with: lister.password
-    within(".profile-form") do
+    within ".profile-form" do
       click_on "Login"
     end
 
@@ -33,7 +33,7 @@ class ListerCanEditProfileTest < ActionDispatch::IntegrationTest
     fill_in "Street address", with: "new_street_address"
     fill_in "City", with: "new_city"
     select "Washington", from: "user_state"
-    fill_in "Zipcode", with: 22222
+    fill_in "Zipcode", with: 22_222
     fill_in "Bio", with: "new_bio"
     fill_in "Password", with: "new_password"
     fill_in "Password confirmation", with: "new_password"
@@ -59,8 +59,52 @@ class ListerCanEditProfileTest < ActionDispatch::IntegrationTest
     refute page.has_content?(original_lister.bio)
   end
 
-  test "only contractors editing their profile see the role dropdown" do
-    skip
+  test "listers editing profile may not change role" do
+    contractor = create(:contractor)
+    lister = create(:lister)
+    visit root_path
+
+    within "#nav-mobile" do
+      click_on "Login"
+    end
+    fill_in "Username", with: contractor.username
+    fill_in "Password", with: contractor.password
+    within ".profile-form" do
+      click_on "Login"
+    end
+
+    assert_equal dashboard_path, current_path
+    assert page.has_content?(contractor.first_name)
+    assert page.has_content?(contractor.last_name)
+    click_on "Edit Profile"
+
+    assert edit_user_path(contractor), current_path
+    within ".profile-form" do
+      assert page.has_select?("Role", options: ["contractor", "lister"])
+    end
+    within "#nav-mobile" do
+      click_on "Logout"
+    end
+
+    assert_equal root_path, current_path
+    within "#nav-mobile" do
+      click_on "Login"
+    end
+    fill_in "Username", with: lister.username
+    fill_in "Password", with: lister.password
+    within ".profile-form" do
+      click_on "Login"
+    end
+
+    assert_equal dashboard_path, current_path
+    assert page.has_content?(lister.first_name)
+    assert page.has_content?(lister.last_name)
+    click_on "Edit Profile"
+
+    assert edit_user_path(lister), current_path
+    within ".profile-form" do
+      assert page.has_select?("Role", options: ["lister"])
+    end
   end
 
   def fixture_image_path
