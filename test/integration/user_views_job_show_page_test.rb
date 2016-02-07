36e07664c0af85026ee87df8ae931993ef1f4dd5
ToniRib <tonimarierib@gmail.com>
2016-02-07 11:33:05 -0700

    Add tests for guest viewing jobs of all statuses

--- test/integration/user_views_job_show_page_test.rb
+++ test/integration/user_views_job_show_page_test.rb
@@ -17,6 +17,62 @@ class UserViewsJobShowPageTest < ActionDispatch::IntegrationTest
     verify_lister_info(lister)
   end
 
+  test "guest views job show page while bidding is closed" do
+    job = create(:job)
+    job.bidding_closed!
+
+    visit user_job_path(job.lister, job)
+
+    refute page.has_css? "#accordion h3", text: "Place A Bid"
+    refute page.has_link? "Log In To Place A Bid", login_path
+    refute page.has_css? "#current-bids"
+
+    assert page.has_css? ".bidding-closed",
+                         text: "Bidding Is Closed For This Job"
+  end
+
+  test "guest views job show page while job is in progress" do
+    job = create(:job)
+    job.in_progress!
+
+    visit user_job_path(job.lister, job)
+
+    refute page.has_css? "#accordion h3", text: "Place A Bid"
+    refute page.has_link? "Log In To Place A Bid", login_path
+    refute page.has_css? "#current-bids"
+
+    assert page.has_css? ".job-in-progress",
+                         text: "Job Is In Progress"
+  end
+
+  test "guest views job show page when job is completed" do
+    job = create(:job)
+    job.completed!
+
+    visit user_job_path(job.lister, job)
+
+    refute page.has_css? "#accordion h3", text: "Place A Bid"
+    refute page.has_link? "Log In To Place A Bid", login_path
+    refute page.has_css? "#current-bids"
+
+    assert page.has_css? ".job-completed",
+                         text: "Job Completed"
+  end
+
+  test "guest views job show page when job has been cancelled" do
+    job = create(:job)
+    job.cancelled!
+
+    visit user_job_path(job.lister, job)
+
+    refute page.has_css? "#accordion h3", text: "Place A Bid"
+    refute page.has_link? "Log In To Place A Bid", login_path
+    refute page.has_css? "#current-bids"
+
+    assert page.has_css? ".job-cancelled",
+                         text: "Job Cancelled"
+  end
+
   test "contractor views job show page while bidding is open" do
     contractor = create(:contractor)
     job = create(:job)
@@ -34,21 +90,6 @@ class UserViewsJobShowPageTest < ActionDispatch::IntegrationTest
 
     verify_job_info(job)
     verify_lister_info(lister)
-  end
-
-  test "guest views job show page while bidding is closed" do
-    job = create(:job)
-    job.bidding_closed!
-    lister = job.lister
-
-    visit user_job_path(job.lister, job)
-
-    refute page.has_css? "#accordion h3", text: "Place A Bid"
-    refute page.has_link? "Log In To Place A Bid", login_path
-    refute page.has_css? "#current-bids"
-
-    assert page.has_css? ".bidding-closed",
-                         text: "Bidding Is Closed For This Job"
   end
 
   test "contractor views job show page while bidding is closed" do
