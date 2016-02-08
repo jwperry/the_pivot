036d1752783a7301fb9ad6808c22da4e1f29de48
Dan Winter <dan.j.winter@gmail.com>
2016-02-08 14:15:33 -0700

    add javascript test capabilities to test_helper
    use method i_need_javascript and pass it a block that needs
    to be tested with javascript

--- app/controllers/user/jobs_controller.rb
+++ app/controllers/user/jobs_controller.rb
@@ -1,7 +1,6 @@
 class User::JobsController < ApplicationController
   def show
     session[:forwarding_url] = request.url
-
     @user = User.find_by_slug(params[:user_slug])
     @job = JobPresenter.new(Job.find(params[:id]), view_context)
   end
