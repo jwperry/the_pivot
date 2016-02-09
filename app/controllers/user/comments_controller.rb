456eeb25f85987b42b78970737e31c55bca333b7
ToniRib <tonimarierib@gmail.com>
2016-02-08 17:20:36 -0700

    User can leave a comment for another user

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -1,4 +1,6 @@
 class User::CommentsController < ApplicationController
+  before_action :update_params, only: [:create]
+
   def new
     @user = current_user
     @job = Job.find(params[:job_id])
@@ -6,5 +8,37 @@ class User::CommentsController < ApplicationController
   end
 
   def create
+    comment = current_user.comments.new(comment_params)
+
+    if comment.save
+      flash[:success] = "Comment Saved!"
+      redirect_to dashboard_path
+    else
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:rating,
+                                    :text,
+                                    :job_id,
+                                    :recipient_id)
+  end
+
+  def update_params
+    params[:comment][:rating] = params[:comment][:rating].to_i
+    params[:comment][:job_id] = params[:job_id]
+    params[:comment][:recipient_id] = find_recipient_id
+  end
+
+  def find_recipient_id
+    job = Job.find(params[:job_id])
+
+    if job.lister == current_user
+      job.contractor_for_selected_bid.id
+    else
+      job.lister.id
+    end
   end
 end
--- test/integration/user_leaves_feedback_test.rb
+++ test/integration/user_leaves_feedback_test.rb
@@ -9,6 +9,27 @@ class UserLeavesFeedbackTest < ActionDispatch::IntegrationTest
     lister = job.lister
     contractor = bid.user
 
+    ApplicationController.any_instance.stubs(:current_user).returns(lister)
 
+    visit new_user_job_comment_path(lister, job)
+
+    select "4", from: "comment_rating"
+    fill_in "comment_text", with: "You were the most amazing contractor I have ever worked with!"
+
+    click_on "Leave Comment"
+
+    assert_equal dashboard_path, current_path
+
+    visit user_path(contractor)
+
+    comment = Comment.last
+
+    within "#previous-jobs" do
+      assert page.has_content? comment.text
+      assert page.has_link? lister.full_name, user_path(lister)
+      assert page.has_content? comment.rating
+      assert page.has_content? comment.posted_at
+      assert page.has_content? comment.accepted_bid_price
+    end
   end
 end
