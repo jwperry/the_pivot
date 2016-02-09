60679edb6ec5f85838b74ec1a26a69c5926d8c72
Joe Perry <joseph.w.perry@gmail.com>
2016-02-08 18:05:10 -0700

    New job saving with datetime objects

--- app/controllers/user/jobs_controller.rb
+++ app/controllers/user/jobs_controller.rb
@@ -6,9 +6,6 @@ class User::JobsController < ApplicationController
   end
 
   def create
-    # new_job_params = job_params
-    # new_job_params["duration_estimate"] = new_job_params["duration_estimate"].to_i
-    byebug
     @job = current_user.jobs.new(job_params)
     if @job.save
       redirect_to dashboard_path
@@ -39,8 +36,20 @@ class User::JobsController < ApplicationController
 
   def sanitize_job_params
     params[:job][:duration_estimate] = params[:job][:duration_estimate].to_i
-    params[:job][:close_hour] = params[:bid_close]["time(4i)"]
-    params[:job][:close_minutes] = params[:bid_close]["time(5i)"]
-    params[:job][:bidding_close_date] = DateTime.new(
+    params[:job][:bidding_close_date] = construct_bidding_close_date
+    params[:job][:must_complete_by_date] = construct_must_complete_by_date
   end
+
+  def construct_bidding_close_date
+    bid_close_date = params[:job][:bidding_close_date]
+    param_hour = params[:bid_close]["time(4i)"]
+    hour, am_pm = Time.parse("#{param_hour}").strftime("%l %P").split(" ")
+    minute = params[:bid_close]["time(5i)"]
+    DateTime.strptime("#{bid_close_date} #{hour}:#{minute} #{am_pm}", "%m/%d/%Y %I:%M %P")
+  end
+
+  def construct_must_complete_by_date
+    complete_by_date = params[:job][:must_complete_by_date]
+    DateTime.strptime("#{complete_by_date}", "%m/%d/%Y")
+  end  
 end
